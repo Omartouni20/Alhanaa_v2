edec0813d30710cfa308d90f7fe823ec0185eb3f
Omartouni300 <omar.sarhan@alhanaaagri.com>
2026-01-16 00:15:41 +0200

    fix: use HashRouter on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import App from "./app/App.tsx";
+import "./styles/index.css";
+
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <App />
